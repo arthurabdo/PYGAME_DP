6697edea97581d36a8fc93a6c67bfe569662e568
Ana Luiza de la Peña <analblp@al.insper.edu.br>
2023-08-01 10:36:52 -0300

    desenhando a cobra

--- Pygame_snake.py
+++ Pygame_snake.py
@@ -5,7 +5,21 @@ import random
 from pygame.math import Vector2
 from random import randint
 
+# desenahndo a cobra
+class COBRA:
+    def __init__(self):
+        self.corpo = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        
+    def desenhar_cobra(self):
+        for bloco in self.corpo: # criando um retangulo 
+            pos_x = int(bloco.x * tamanho) # posição x
+            pos_y = int(bloco.y * tamanho) # posição y
+            bloco_rect = pygame.Rect(pos_x, pos_y, tamanho, tamanho)
+            pygame.draw.rect(tela, (230, 230, 250), bloco_rect) # desenhando o retangulo -- cobra 
+            # é draw ou desenhar?
 
+
+# desenhando a fruta, que alimenta a cobra e onde ela vai aparecer na tela 
 class PESSEGO:
     def __init__(self):
         self.x = randint(0, numero - 1)
@@ -26,6 +40,7 @@ numero = 20
 tela = pygame.display.set_mode((numero * tamanho, numero * tamanho))
 
 pessego = PESSEGO()
+cobra = COBRA()
 
 # loop do jogo
 while True:
@@ -36,5 +51,6 @@ while True:
             sys.exit()
     tela.fill((2,15,60))
     pessego.desenhar_pessego()
+    cobra.desenhar_cobra()
     pygame.display.update()
     fps.tick(120)
